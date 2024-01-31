2ccb559efcb08b3d6626fa07924c42542d2924d4
Farhad Khan Haadi <89342676+FARHADGULL@users.noreply.github.com>
2024-01-31 15:10:05 +0500

    Update pick_image.dart

--- lib/widgets/custom_image_widget/pick_image.dart
+++ lib/widgets/custom_image_widget/pick_image.dart
@@ -6,7 +6,6 @@ import 'package:flutter_image_compress/flutter_image_compress.dart';
 import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:image_picker/image_picker.dart';
-import 'package:ismmart_ecommerce/helpers/app_colors.dart';
 import 'package:ismmart_ecommerce/helpers/common_function.dart';
 import 'package:path_provider/path_provider.dart' as path_provider;
 
