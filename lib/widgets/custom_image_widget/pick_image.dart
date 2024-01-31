90478343ffec0ff717ddc70e1ee32f7f34db5a32
Farhad Khan Haadi <89342676+FARHADGULL@users.noreply.github.com>
2024-01-31 15:09:53 +0500

    Update pick_image.dart

--- lib/widgets/custom_image_widget/pick_image.dart
+++ lib/widgets/custom_image_widget/pick_image.dart
@@ -7,6 +7,7 @@ import 'package:get/get.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:ismmart_ecommerce/helpers/app_colors.dart';
+import 'package:ismmart_ecommerce/helpers/common_function.dart';
 import 'package:path_provider/path_provider.dart' as path_provider;
 
 import '../custom_snackbar.dart';
@@ -116,7 +117,7 @@ class PickImage {
     double length = await checkImageSize(result!);
 
     if (length > 2) {
-      CustomSnackBar.showSnackBar(
+      CommonFunction.showSnackBar(
           title: 'Error', message: "Image Size must be less than 2MB");
       return null;
     }
@@ -226,10 +227,10 @@ class PickImage {
         double length = await checkImageSize(result!);
 
         if (length > 2) {
-          CustomSnackBar.showSnackBar(
-              title: "Error",
-              message: "image size should be less then 2MB",
-              color: AppColors.red);
+          CommonFunction.showSnackBar(
+            title: "Error",
+            message: "image size should be less then 2MB",
+          );
         } else {
           File file = File(result.path);
           tempList.add(file);
