12d64bb958efaec6817b93e172e845512f2c72e1
Farhad Khan Haadi <89342676+FARHADGULL@users.noreply.github.com>
2024-01-31 12:54:06 +0500

    Update home_view.dart

--- lib/screens/home/home_view.dart
+++ lib/screens/home/home_view.dart
@@ -1,10 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:ismmart_ecommerce/helpers/common_function.dart';
+
+import '../../widgets/custom_button.dart';
 
 class HomeView extends StatelessWidget {
   const HomeView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('ISMMART eCommerce'),
+      ),
+      body: Center(
+        child: CustomIconTextBtn(
+          title: 'CustomIconTextBtn',
+          onPressed: () {
+            CommonFunction.showBottomSheet(context: context);
+          },
+          icon: Icons.add,
+        ),
+      ),
+    );
   }
 }
