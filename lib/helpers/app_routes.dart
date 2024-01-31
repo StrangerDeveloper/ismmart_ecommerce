6ee9ef7f15a7001835651e63fe6f3c574e9b4743
strangerdeveloper <strangerdeveloper081@gmail.com>
2024-01-31 11:27:20 +0500

    widgets and screens integrated

---
+++ lib/helpers/app_routes.dart
@@ -0,0 +1,12 @@
+abstract class AppRouter {
+  List getRoutes();
+}
+
+class AppRoutes extends AppRouter {
+  @override
+  List getRoutes() {
+    // TODO: implement getRoutes
+    throw UnimplementedError();
+  }
+
+}
