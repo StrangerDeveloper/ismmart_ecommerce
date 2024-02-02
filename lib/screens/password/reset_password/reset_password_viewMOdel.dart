d092bf0f704f019d5f0c342c38159b6158395988
Farhad Khan Haadi <89342676+FARHADGULL@users.noreply.github.com>
2024-02-02 12:45:02 +0500

    Update reset_password_viewMOdel.dart

--- lib/screens/password/reset_password/reset_password_viewMOdel.dart
+++ lib/screens/password/reset_password/reset_password_viewMOdel.dart
@@ -2,5 +2,5 @@ import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
 class ResetPasswordViewModel extends GetxController {
-  TextEditingController emailController = TextEditingController();
+  TextEditingController passwordController = TextEditingController();
 }
