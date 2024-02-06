7a432c037969dffd04ba736ee8b1f994735590b3
Farhad Khan Haadi <89342676+FARHADGULL@users.noreply.github.com>
2024-02-06 14:46:08 +0500

    Update wishlist_view.dart

--- lib/screens/wishlist/wishlist_view.dart
+++ lib/screens/wishlist/wishlist_view.dart
@@ -45,7 +45,7 @@ class WishlistView extends StatelessWidget {
         child: Column(
           children: [
             const Padding(
-              padding: EdgeInsets.symmetric(horizontal: 16, vertical: 16),
+              padding: EdgeInsets.symmetric(horizontal: 16, vertical: 20),
               child: Align(
                 alignment: Alignment.centerLeft,
                 child: CustomText(
