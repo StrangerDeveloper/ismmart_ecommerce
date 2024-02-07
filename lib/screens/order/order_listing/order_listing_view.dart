ca72adc3dfefb16ab8e8e54218aeafb2f599ecc9
Farhad Khan Haadi <89342676+FARHADGULL@users.noreply.github.com>
2024-02-07 10:58:08 +0500

    Create order_listing_view.dart

---
+++ lib/screens/order/order_listing/order_listing_view.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/cupertino.dart';
+
+class OrderListingView extends StatelessWidget {
+  const OrderListingView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
