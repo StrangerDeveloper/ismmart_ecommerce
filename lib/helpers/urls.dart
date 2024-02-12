1310c43d7b03742e473fa41c8acb8a9edd0a25c1
hayatbangash <hayatbangash66@gmail.com>
2024-02-12 12:56:51 +0500

    home screen -- backend integration.

--- lib/helpers/urls.dart
+++ lib/helpers/urls.dart
@@ -3,4 +3,7 @@ class Urls {
       "https://ismmart-ecommerce-backend-e233368b3b0d.herokuapp.com/api";
   static String getLocation = "/vendor/location?limit=5&page=";
   static String getOrders = "/vendor/order";
+
+  //HomeViewModel
+  static String homeCollections = "/public/collection?limit=0&level=1&fields[name]=1&fields[media]=1&fields[children]=1";
 }
