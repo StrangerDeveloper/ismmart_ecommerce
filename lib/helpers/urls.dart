58b0fdf95af038037d80e70479b86368fbf0a335
hayatbangash <hayatbangash66@gmail.com>
2024-02-12 18:11:28 +0500

    home screen -- backend integration.

--- lib/helpers/urls.dart
+++ lib/helpers/urls.dart
@@ -16,8 +16,6 @@ class Urls {
   static String getNews = "/public/news?limit=0&fields[name]=1&fields[description]=1";
   static String getDiscount = "/public/discount?limit=6&fields[name]=1&fields[start]=1&fields[end]=1";
   static String getDiscountedProducts = "/public/product?limit=6&fields[image]=1&fields[quantity]=1&fields[inStock]=1&fields[store][name]=1&fields[discount][percentage]=1&fields[totalReviews]=1&fields[rating]=1&fields[price]=1&discount=";
-  static String getDiscount = "/public/discount?limit=6&fields[name]=1&fields[start]=1&fields[end]=1&collection=";
-  static String getDiscountedProducts = "/public/product?limit=6&fields[image]=1&fields[name]=1&fields[quantity]=1&fields[inStock]=1&fields[store][name]=1&fields[discount][percentage]=1&fields[totalReviews]=1&fields[rating]=1&fields[price]=1&discount=";
   // static String getSimpleProducts = "/public/product?limit=6&fields[image]=1&fields[name]=1&fields[quantity]=1&fields[inStock]=1&fields[store][name]=1&fields[discount][percentage]=1&fields[totalReviews]=1&fields[rating]=1&fields[price]=1&limit=10&collection=";
   static String getSimpleProducts = "/public/product?limit=6&fields[image]=1&fields[quantity]=1&fields[name]=1&fields[inStock]=1&fields[store][name]=1&fields[discount][percentage]=1&fields[totalReviews]=1&fields[rating]=1&fields[price]=1&limit=10";
 
