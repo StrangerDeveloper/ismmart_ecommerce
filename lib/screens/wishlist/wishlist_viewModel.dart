ba0d3c94cc250d8a201a31f26f37ce64c2cbc70e
Farhad Khan Haadi <89342676+FARHADGULL@users.noreply.github.com>
2024-02-14 11:06:57 +0500

    Update wishlist_viewModel.dart

--- lib/screens/wishlist/wishlist_viewModel.dart
+++ lib/screens/wishlist/wishlist_viewModel.dart
@@ -1,4 +1,7 @@
 import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
+
+import '../product_details/product_model.dart';
 
 class WishlistViewModel extends GetxController {
   List<String> categoriesList = <String>[
@@ -9,4 +12,28 @@ class WishlistViewModel extends GetxController {
     'https://images.unsplash.com/photo-1528162771413-a34de3292ab1?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
     'https://images.unsplash.com/photo-1508833876629-23d0873cfa06?q=80&w=1631&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
   ].obs;
+
+  static final localStorage = GetStorage();
+  RxList<Product> wishlist = <Product>[].obs;
+
+  // Retrieve wishlist from local storage during initialization
+  @override
+  void onInit() {
+    super.onInit();
+    List<Product> savedWishlist =
+        localStorage.read<List<Product>>('wishlist') ?? [];
+    wishlist.assignAll(savedWishlist);
+  }
+
+  void addToWishlist(Product product) {
+    print('object: ${product.name}');
+    wishlist.add(product);
+    localStorage.write('wishlist', wishlist.toList());
+    print('wishlist nameeeeee: ${wishlist[0].name}');
+  }
+
+  void removeFromWishlist(Product product) {
+    wishlist.remove(product);
+    localStorage.write('wishlist', wishlist.toList());
+  }
 }
