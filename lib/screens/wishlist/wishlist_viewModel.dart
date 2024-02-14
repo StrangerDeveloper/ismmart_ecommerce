0b9c8c44be0047aaf589d31f9e6c911d300bbaaf
Farhad Khan Haadi <89342676+FARHADGULL@users.noreply.github.com>
2024-02-14 14:58:07 +0500

    Update wishlist_viewModel.dart

--- lib/screens/wishlist/wishlist_viewModel.dart
+++ lib/screens/wishlist/wishlist_viewModel.dart
@@ -4,17 +4,9 @@ import 'package:get_storage/get_storage.dart';
 import '../product_details/product_model.dart';
 
 class WishlistViewModel extends GetxController {
-  List<String> categoriesList = <String>[
-    'https://images.unsplash.com/photo-1575936123452-b67c3203c357?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-    'https://images.unsplash.com/photo-1528162771413-a34de3292ab1?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-    'https://images.unsplash.com/photo-1508833876629-23d0873cfa06?q=80&w=1631&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-    'https://images.unsplash.com/photo-1575936123452-b67c3203c357?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-    'https://images.unsplash.com/photo-1528162771413-a34de3292ab1?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-    'https://images.unsplash.com/photo-1508833876629-23d0873cfa06?q=80&w=1631&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
-  ].obs;
-
   static final localStorage = GetStorage();
   RxList<Product> wishlist = <Product>[].obs;
+  RxInt total = 0.obs;
 
   // Retrieve wishlist from local storage during initialization
   @override
@@ -28,12 +20,16 @@ class WishlistViewModel extends GetxController {
   void addToWishlist(Product product) {
     print('object: ${product.name}');
     wishlist.add(product);
+    wishlist.refresh();
+    total.value = wishlist.length;
     localStorage.write('wishlist', wishlist.toList());
     print('wishlist nameeeeee: ${wishlist[0].name}');
   }
 
   void removeFromWishlist(Product product) {
     wishlist.remove(product);
+    wishlist.refresh();
+    total.value = wishlist.length;
     localStorage.write('wishlist', wishlist.toList());
   }
 }
