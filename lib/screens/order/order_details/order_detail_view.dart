a53d6683c696f5ce143d765738cd466c2cf0a42c
Farhad Khan Haadi <89342676+FARHADGULL@users.noreply.github.com>
2024-02-15 12:51:20 +0500

    Update order_detail_view.dart

--- lib/screens/order/order_details/order_detail_view.dart
+++ lib/screens/order/order_details/order_detail_view.dart
@@ -332,80 +332,127 @@ class OrderDetailView extends StatelessWidget {
         children: [
           _customField2("Order Information"),
           const SizedBox(height: 15),
-          Row(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Expanded(
-                child: _customField1(viewModel.orderItemModel.value
-                        .lineitems?[0].assignedRider?.name ??
-                    "Shipping Method:"),
-              ),
-              Expanded(
-                child: _customField2(viewModel.orderItemModel.value
-                        .lineitems?[0].assignedRider?.cnic ??
-                    "Home, Chino Hills, CA 91709, USA"),
-              ),
-            ],
+          _customOrderInfo(
+            text1: "Shipping Address:",
+            text2: viewModel
+                    .orderItemModel.value.lineitems?[0].assignedRider?.cnic ??
+                "Home, Chino Hills, CA 91709, USA",
           ),
-          Row(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Expanded(
-                child: _customField1(viewModel.orderItemModel.value
-                        .lineitems?[0].assignedRider?.name ??
-                    "Payment Method:"),
-              ),
-              Expanded(
-                child: _customField2(viewModel.orderItemModel.value
-                        .lineitems?[0].assignedRider?.cnic ??
-                    "COD"),
-              ),
-            ],
+          // Row(
+          //   crossAxisAlignment: CrossAxisAlignment.start,
+          //   children: [
+          //     Expanded(
+          //       child: _customField1(viewModel.orderItemModel.value
+          //               .lineitems?[0].assignedRider?.name ??
+          //           "Shipping Method:"),
+          //     ),
+          //     Expanded(
+          //       child: _customField2(viewModel.orderItemModel.value
+          //               .lineitems?[0].assignedRider?.cnic ??
+          //           "Home, Chino Hills, CA 91709, USA"),
+          //     ),
+          //   ],
+          // ),
+          _customOrderInfo(
+            text1: "Payment Method:",
+            text2: viewModel
+                    .orderItemModel.value.lineitems?[0].assignedRider?.name ??
+                "COD",
           ),
-          Row(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Expanded(
-                child: _customField1(viewModel.orderItemModel.value
-                        .lineitems?[0].assignedRider?.name ??
-                    "Delivery Method:"),
-              ),
-              Expanded(
-                child: _customField2(viewModel.orderItemModel.value
-                        .lineitems?[0].assignedRider?.cnic ??
-                    "Leopards, 3 days, 15"),
-              ),
-            ],
+          // Row(
+          //   crossAxisAlignment: CrossAxisAlignment.start,
+          //   children: [
+          //     Expanded(
+          //       child: _customField1(viewModel.orderItemModel.value
+          //               .lineitems?[0].assignedRider?.name ??
+          //           "Payment Method:"),
+          //     ),
+          //     Expanded(
+          //       child: _customField2(viewModel.orderItemModel.value
+          //               .lineitems?[0].assignedRider?.cnic ??
+          //           "COD"),
+          //     ),
+          //   ],
+          // ),
+          _customOrderInfo(
+            text1: "Delivery Method:",
+            text2: viewModel
+                    .orderItemModel.value.lineitems?[0].assignedRider?.cnic ??
+                "Leopards, 3 days, 15",
           ),
-          Row(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Expanded(
-                child: _customField1(viewModel.orderItemModel.value
-                        .lineitems?[0].assignedRider?.name ??
-                    "Discount:"),
-              ),
-              Expanded(
-                child: _customField2(
-                    viewModel.orderItemModel.value.deliveryStatus ??
-                        "10%, Personal promo code"),
-              ),
-            ],
+          // Row(
+          //   crossAxisAlignment: CrossAxisAlignment.start,
+          //   children: [
+          //     Expanded(
+          //       child: _customField1(viewModel.orderItemModel.value
+          //               .lineitems?[0].assignedRider?.name ??
+          //           "Delivery Method:"),
+          //     ),
+          //     Expanded(
+          //       child: _customField2(viewModel.orderItemModel.value
+          //               .lineitems?[0].assignedRider?.cnic ??
+          //           "Leopards, 3 days, 15"),
+          //     ),
+          //   ],
+          // ),
+          _customOrderInfo(
+            text1: "Discount:",
+            text2: viewModel
+                    .orderItemModel.value.lineitems?[0].assignedRider?.cnic ??
+                "10%, Personal promo code",
           ),
-          Row(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Expanded(
-                child: _customField1(viewModel.orderItemModel.value
-                        .lineitems?[0].assignedRider?.name ??
-                    "Total Amount:"),
-              ),
-              Expanded(
-                child: _customField2(
-                    viewModel.orderItemModel.value.deliveryStatus ?? "133"),
-              ),
-            ],
-          )
+          // Row(
+          //   crossAxisAlignment: CrossAxisAlignment.start,
+          //   children: [
+          //     Expanded(
+          //       child: _customField1(viewModel.orderItemModel.value
+          //               .lineitems?[0].assignedRider?.name ??
+          //           "Discount:"),
+          //     ),
+          //     Expanded(
+          //       child: _customField2(
+          //           viewModel.orderItemModel.value.deliveryStatus ??
+          //               "10%, Personal promo code"),
+          //     ),
+          //   ],
+          // ),
+          _customOrderInfo(
+            text1: "Total Amount:",
+            text2: viewModel
+                    .orderItemModel.value.lineitems?[0].assignedRider?.cnic ??
+                "133",
+          ),
+          // Row(
+          //   crossAxisAlignment: CrossAxisAlignment.start,
+          //   children: [
+          //     Expanded(
+          //       child: _customField1(viewModel.orderItemModel.value
+          //               .lineitems?[0].assignedRider?.name ??
+          //           "Total Amount:"),
+          //     ),
+          //     Expanded(
+          //       child: _customField2(
+          //           viewModel.orderItemModel.value.deliveryStatus ?? "133"),
+          //     ),
+          //   ],
+          // )
+        ],
+      ),
+    );
+  }
+
+  Widget _customOrderInfo({required String text1, required String text2}) {
+    return Padding(
+      padding: const EdgeInsets.only(top: 8.0),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Expanded(
+            child: _customField1(text1),
+          ),
+          Expanded(
+            child: _customField2(text2),
+          ),
         ],
       ),
     );
