7af80099f5e0fbb4b2a60501363672f43d8037d9
hayatbangash <hayatbangash66@gmail.com>
2024-02-16 15:25:56 +0500

    search & searchDetail screen -- backend integration.

--- lib/screens/category/category_view.dart
+++ lib/screens/category/category_view.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
+import 'package:ismmart_ecommerce/helpers/app_routes.dart';
 import 'package:ismmart_ecommerce/helpers/global_variables.dart';
 import 'package:ismmart_ecommerce/screens/category/category_viewmodel.dart';
-import 'package:ismmart_ecommerce/screens/sub_category/sub_category_view.dart';
 import 'package:ismmart_ecommerce/widgets/loader_view.dart';
 
 import '../../widgets/custom_network_image.dart';
@@ -40,10 +40,12 @@ class CategoryView extends StatelessWidget {
                             return InkWell(
                               borderRadius: BorderRadius.circular(4),
                               onTap: () {
-                                Get.to(() => SubCategoryView(), arguments: {
+                                Get.toNamed(AppRoutes.subCategory, arguments: {
                                   'id':
                                       viewModel.categoriesList[index].sId ?? ''
-                                });
+                                }
+
+                                );
                               },
                               child: Padding(
                                 padding: const EdgeInsets.symmetric(
@@ -54,7 +56,8 @@ class CategoryView extends StatelessWidget {
                                       child: CustomNetworkImage(
                                         imageUrl: viewModel
                                                 .categoriesList[index]
-                                                .media?.first ??
+                                                .media
+                                                ?.first ??
                                             '',
                                         shape: BoxShape.circle,
                                       ),
@@ -148,42 +151,40 @@ class CategoryView extends StatelessWidget {
     );
   }
 
-  Widget collectionListViewItem(int index){
+  Widget collectionListViewItem(int index) {
     return Obx(() => Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 5),
-      child: Material(
-        color: Colors.transparent,
-        child: InkWell(
-          onTap: () {
-            viewModel.changeCollection(index);
-          },
-          child: Container(
-            decoration: BoxDecoration(
-              border:
-              viewModel.collectionCurrentIndex.value == index
-                  ? const Border(
-                bottom: BorderSide(
-                  width: 2,
-                  color: Colors.black,
+          padding: const EdgeInsets.symmetric(horizontal: 5),
+          child: Material(
+            color: Colors.transparent,
+            child: InkWell(
+              onTap: () {
+                viewModel.changeCollection(index);
+              },
+              child: Container(
+                decoration: BoxDecoration(
+                  border: viewModel.collectionCurrentIndex.value == index
+                      ? const Border(
+                          bottom: BorderSide(
+                            width: 2,
+                            color: Colors.black,
+                          ),
+                        )
+                      : null,
                 ),
-              )
-                  : null,
-            ),
-            padding: const EdgeInsets.symmetric(horizontal: 2),
-            alignment: Alignment.center,
-            child: Text(
-              GlobalVariable.collectionList[index].name
-                  ?.capitalizeFirst ??
-                  '',
-              style: const TextStyle(
-                fontSize: 12,
-                color: Colors.black,
-                fontWeight: FontWeight.w500,
+                padding: const EdgeInsets.symmetric(horizontal: 2),
+                alignment: Alignment.center,
+                child: Text(
+                  GlobalVariable.collectionList[index].name?.capitalizeFirst ??
+                      '',
+                  style: const TextStyle(
+                    fontSize: 12,
+                    color: Colors.black,
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
               ),
             ),
           ),
-        ),
-      ),
-    ));
+        ));
   }
 }
