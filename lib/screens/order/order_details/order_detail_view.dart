f512caa92f590cd1e836f7f47d4a870e0d43afcb
Farhad Khan Haadi <89342676+FARHADGULL@users.noreply.github.com>
2024-02-22 22:00:59 +0500

    Update order_detail_view.dart

--- lib/screens/order/order_details/order_detail_view.dart
+++ lib/screens/order/order_details/order_detail_view.dart
@@ -230,8 +230,6 @@ class OrderDetailView extends StatelessWidget {
                               }).toList() ??
                               [],
 
-                          //     width: 16,
-                          //   ),
                           //   _customField2(
                           //       "Rs. ${model.totals?.total?.toStringAsFixed(2) ?? "total"}"),
                           // ],
